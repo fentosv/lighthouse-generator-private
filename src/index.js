51565ac9b03124cf96b9e2e6c52bd0c078ea4ac2
fentosv <fentos.dev@gmail.com>
2023-02-14 20:10:42 +0100

    Test

--- src/index.js
+++ src/index.js
@@ -1,21 +1,5 @@
-import core from '@actions/core'
-import getLighthouseReport from './getLighthouseReport'
-
 async function run() {
-  try {
-    const badgeStyle = core.getInput('badgeStyle')
-    const commitAuthor = core.getInput('commitAuthor')
-    const commitMessage = core.getInput('commitMessage')
-    const mdName = core.getInput('mdName')
-    const url = core.getInput('url')
-    core.info(`Getting Lighthouse report for ${url}...`)
-
-    getLighthouseReport({ url, badgeStyle, mdName })
-    core.info(new Date().toTimeString())
-    core.setOutput('time', new Date().toTimeString())
-  } catch (error) {
-    core.setFailed(error.message)
-  }
+  console.log('hola')
 }
 
 run()
